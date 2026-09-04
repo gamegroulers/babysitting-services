6bbdcd238fe230ccf71ad1d8676b31c0168f99f8
gamegroulers <131318140+gamegroulers@users.noreply.github.com>
2026-09-04 04:53:49 -0700

    Correct typo in Firebase initialization
    Fix typo in admin.initializeApp function.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  
   admin.initializeeApp({
     credential: admin.credential.cert(serviceAccount)
   });
